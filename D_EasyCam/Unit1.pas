2cde6a4bfaa74c07b85a2be4ad2f77a8c173d99a
grantlj <393444163@qq.com>
2013-10-17 07:41:19 +0800

    Test _Android_Camera
    by delphi xe5.

---
+++ D_EasyCam/Unit1.pas
@@ -0,0 +1,46 @@
+unit Unit1;
+
+interface
+
+uses
+  System.SysUtils, System.Types, System.UITypes, System.Classes, System.Variants,
+  FMX.Types, FMX.Controls, FMX.Forms, FMX.Graphics, FMX.Dialogs, System.Actions,
+  FMX.ActnList, FMX.StdActns, FMX.MediaLibrary.Actions, FMX.Objects,
+  FMX.StdCtrls;
+
+type
+  TForm1 = class(TForm)
+    ToolBar1: TToolBar;
+    Button1: TButton;
+    Button2: TButton;
+    Button3: TButton;
+    Image1: TImage;
+    ActionList1: TActionList;
+    TakePhotoFromCameraAction1: TTakePhotoFromCameraAction;
+    ShowShareSheetAction1: TShowShareSheetAction;
+    procedure TakePhotoFromCameraAction1DidFinishTaking(Image: TBitmap);
+    procedure ShowShareSheetAction1BeforeExecute(Sender: TObject);
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.fmx}
+
+procedure TForm1.ShowShareSheetAction1BeforeExecute(Sender: TObject);
+begin
+  ShowShareSheetAction1.Bitmap.Assign(Image1.Bitmap);
+end;
+
+procedure TForm1.TakePhotoFromCameraAction1DidFinishTaking(Image: TBitmap);
+begin
+   Image1.Bitmap.Assign(Image);
+end;
+
+end.
